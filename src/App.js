10f20d5b0affb153ef7a2639f929ff16779a1770
uves-shaikh <suves58@gmail.com>
2023-09-27 21:12:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import cakegif from './cakegif.gif'
+// import IMG1 from './img1.png'
+// import IMG2 from './img2.png'
+// import IMG3 from './img3.png'
+import IMG1 from './img1.jpg'
+import IMG2 from './img2.jpg'
+import IMG3 from './img3.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class="container">
+        <div class="card">
+            <div class="outside">
+                <h1>Happy Birthday</h1>
+                <h3>Dear Shayra,may god bless you with abundant joy on your birthday and always!
+                    Thank you for always being by my side and having my back.</h3>
+            </div>
+            <div class="inside">
+                <img src={cakegif} alt="cakephptp" />
+                <h3>Sending you an infinite amount of love, joy, and happiness on your birthday!
+                </h3>
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="frames frame1">
+                <img src={IMG1} alt="" />
+                <p>Keep smiling,Keep shining</p>
+            </div>
+            <div class="frames frame2">
+                <img src={IMG2} alt="" />
+                <p>On this occasion bring you lot of happiness and success
+                </p>
+            </div>
+            <div class="frames frame3">
+                <img src={IMG3} alt="" />
+                <p>May God bless you on your birthday, and always</p>
+            </div>
+        </div>
+
+    </div>
     </div>
   );
 }
